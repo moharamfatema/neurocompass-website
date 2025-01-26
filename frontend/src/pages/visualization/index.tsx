95fc5618bf17958df42f1f0fb0ba4819900e82b7
Fatema Moharam <moharamfatema@gmail.com>
2025-01-26 16:17:12 +0100

    opt

--- frontend/src/pages/visualization/index.tsx
+++ frontend/src/pages/visualization/index.tsx
@@ -29,8 +29,25 @@ const ChartTypeTabs: React.FC<{
     );
 };
 
-const Visualization = () => {
+const Charts: React.FC = () => {
     const [selectedTab, setSelectedTab] = React.useState(0);
+
+    return (
+        <React.Fragment>
+            <ChartTypeTabs
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+            />
+            <div className="grow text-center align-middle border border-zinc-500 rounded-lg">
+                {/* visualization */}
+                {selectedTab === 0 && <Regions />}
+                {selectedTab === 1 && <Scores />}
+            </div>
+        </React.Fragment>
+    );
+};
+
+const Visualization = () => {
     return (
         <div className=" flex flex-col items-center justify-center gap-2 m-5 max-h-full">
             <section className="flex w-full rounded-lg bg-zinc-900 h-full">
@@ -40,15 +57,7 @@ const Visualization = () => {
                 <main className="flex flex-col w-full p-4 h-full max-h-[80dvh] overflow-y-auto gap-2">
                     {/* data summary cards */}
                     <DataSummary />
-                    <ChartTypeTabs
-                        selectedTab={selectedTab}
-                        setSelectedTab={setSelectedTab}
-                    />
-                    <div className="grow text-center align-middle border border-zinc-500 rounded-lg">
-                        {/* visualization */}
-                        {selectedTab === 0 && <Regions />}
-                        {selectedTab === 1 && <Scores />}
-                    </div>
+                    <Charts />
                 </main>
             </section>
         </div>
